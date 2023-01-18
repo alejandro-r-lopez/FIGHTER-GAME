31c887c2dd01511d32146879b2b1b453c743d692
ALEJANDRO RAFAEL LOPEZ <alejandroxrxlopez@gmail.com>
2023-01-18 15:07:45 -0500

    default mobs spawning

--- app.js
+++ app.js
@@ -9,3 +9,47 @@
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
+
+const heroImg = document.getElementById('hero-img');
+
+const mobInput = document.getElementById('input');
+const mobButton = document.getElementById('mob-button');
+const mobSpawn = document.getElementById('mob-spawn');
+
+const mobs = [
+    {
+        name: 'greg',
+        hp: 5,
+    },
+    {
+        name: 'mark',
+        hp: 5,
+    },
+    {
+        name: 'tim',
+        hp: 5,
+    },
+];
+
+displayMobs();
+
+function displayMobs() {
+    for (let mob of mobs) {
+        const newMob = renderMobs(mob);
+        mobSpawn.append(newMob);
+    }
+}
+
+function renderMobs(mobs) {
+    const newMob = document.createElement('li');
+    const mobName = document.createElement('p');
+    const mobHP = document.createElement('p');
+
+    mobName.textContent = mobs.name;
+    mobHP.textContent = mobs.hp;
+
+    newMob.classList.add('mob-style');
+
+    newMob.append(mobName, mobHP);
+    return newMob;
+}
