e19bde298b4514bcc628a41a043fda7ffe51f2c8
ALEJANDRO RAFAEL LOPEZ <alejandroxrxlopez@gmail.com>
2023-01-18 15:25:03 -0500

    styled new mobs

--- app.js
+++ app.js
@@ -44,12 +44,14 @@ function renderMobs(mobs) {
     const newMob = document.createElement('li');
     const mobName = document.createElement('p');
     const mobHP = document.createElement('p');
+    const mobImg = document.createElement('img');
 
     mobName.textContent = mobs.name;
     mobHP.textContent = mobs.hp;
+    mobImg.src = './assets/mob-three.png';
 
     newMob.classList.add('mob-style');
 
-    newMob.append(mobName, mobHP);
+    newMob.append(mobName, mobHP, mobImg);
     return newMob;
 }
