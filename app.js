96cc3284fd2199188d6c7dacf35f7c30568f51a0
ALEJANDRO RAFAEL LOPEZ <alejandroxrxlopez@gmail.com>
2023-01-18 16:00:48 -0500

    player HP state working

--- app.js
+++ app.js
@@ -11,11 +11,13 @@
 // (don't forget to call any display functions you want to run on page load!)
 import { renderMobs } from '/utils.js';
 const heroImg = document.getElementById('hero-img');
+const heroHP = document.getElementById('hero-hp');
 
 const mobInput = document.getElementById('input');
 const mobButton = document.getElementById('mob-button');
 const mobSpawn = document.getElementById('mob-spawn');
 
+let HP = 10;
 const mobs = [
     {
         name: 'greg',
@@ -46,6 +48,14 @@ function displayMobs() {
             } else {
                 alert(`You missed ${mob.name}`);
             }
+            if (Math.random() > 0.5) {
+                alert(`${mob.name} hit you!`);
+                HP--;
+            } else {
+                alert(`${mob.name} missed!`);
+            }
+            heroHP.textContent = HP;
+
             displayMobs();
         });
     }
